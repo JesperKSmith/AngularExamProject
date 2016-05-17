f5b3ad3df2f9ca0569b3f6ce61c65039cce23304
ThomasGross <thomasgrossrasmussen@gmail.com>
2016-05-17 19:54:37 +0200

    css styling

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,25 @@
  */
 var gulp = require('gulp'); //tool
 var less = require('gulp-less'); //library for compiling less -> css
-var removeHtmlComments = require('gulp-remove-html-comments');
-var minify = require('gulp-clean-css');
+//var removeHtmlComments = require('gulp-remove-html-comments');
+//var minify = require('gulp-clean-css');
 
 
 gulp.task('less', function() {
-    gulp.src('css.less')
+    gulp.src('app/styles.less')
         .pipe(less())
-        .pipe(minify())
+        //.pipe(minify())
         .pipe(gulp.dest('www/css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('menu.less', ['less']);
+    gulp.watch('app/styles.less', ['less']);
     gulp.watch('*.html', ['html']);
 });
 
 gulp.task('html', function () {
     return gulp.src(['*.html', 'app/partials/*.html'])
-        .pipe(removeHtmlComments())
+        //.pipe(removeHtmlComments())
         .pipe(gulp.dest('www'));
 
 });
