05524c6ce8009140b0833634ebf6d7c32ed8440f
Christian Vang <chrvang89@gmail.com>
2016-05-18 21:07:04 +0200

    Add search filter to animalTable

---
+++ app/filters.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Vang on 18/05/2016.
+ */
+
+angular.module("doggycloud")
+    .filter("animalSearch", function(){
+        return function(data, search){
+
+            if (search === undefined) {
+                return data;
+            } else {
+
+                var result = _.filter(data, function (customer){
+                    return customer.name && customer.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
+                        || customer.breed && customer.breed.toLowerCase().indexOf(search.toLowerCase()) !== -1
+                        || customer.gender && customer.gender.toLowerCase().indexOf(search.toLowerCase()) !== -1
+                        || customer.age && customer.age.toString().indexOf(search) !== -1
+                        || customer.inHeat && customer.inHeat.toLowerCase().indexOf(search.toLowerCase()) !== -1
+                        || customer.bestAlone && customer.bestAlone.toLowerCase().indexOf(search.toLowerCase()) !== -1
+                        || customer.comments && customer.comments.toLowerCase().indexOf(search.toLowerCase()) !== -1});
+
+                return result;
+            }
+        };
+    });
